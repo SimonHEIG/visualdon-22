930b63441e6480537529034fec9705e036f46462
SimonHEIG <simon.meia@heig-vd.ch>
2022-03-11 17:14:53 +0100

    Fin ex2. Translate not working :(

--- 02-intro-d3/src/index.js
+++ 02-intro-d3/src/index.js
@@ -1,13 +1,50 @@
 import * as d3 from 'd3';
 
-// C'est ici que vous allez écrire les premières lignes en d3!
-let svg = d3.select('#circles').append('svg').attr('width', '500').attr('height', '500')
-svg.append('circle').attr("cx", "50").attr("cy", "50").attr("r", "40")
-svg.append('circle').attr("cx", "150").attr("cy", "150").attr("r", "40")
-svg.append('circle').attr("cx", "250").attr("cy", "250").attr("r", "40")
+let circles = d3.select('#circles').append('svg').attr('height', 500).attr('width', 500);
+circles.append('svg').attr('id', 'cercle1').append('circle').attr("cx", "50").attr("cy", "50").attr("r", "40")
+circles.append('svg').attr('id', 'cercle2').append('circle').attr("cx", "150").attr("cy", "150").attr("r", "40")
+circles.append('svg').attr('id', 'cercle3').append('circle').attr("cx", "250").attr("cy", "250").attr("r", "40")
 
-d3.selectAll('circle[cx="150"]').attr('fill', 'red')
+d3.selectAll('#cercle2 circle').attr('fill', 'red')
 
-d3.selectAll('circle[cx="50"]').attr('cx', '100')
-d3.selectAll('circle[cx="150"]').attr('cx', '200')
+d3.selectAll('#cercle1').attr('cx', '100')
+d3.selectAll('#cercle2').attr('cx', '200')
 
+// Texte
+d3.selectAll('circle').each(function () {
+    d3.select(this.parentNode).append("text")
+        .text('Circle')
+        .attr("x", d3.select(this.parentNode).node().getBBox().width - 60)
+        .attr("y", d3.select(this.parentNode).node().getBBox().height)
+})
+
+// translate
+d3.selectAll('#cercle3').on("click", () => {
+    d3.selectAll('#cercle1').attr("transform", "translate (50,0)");
+    d3.selectAll('#cercle2').attr("transform", "translate (150,0)");
+    console.log("test");
+})
+
+
+//Graphique
+const data = [20, 5, 25, 8, 15]
+
+d3.select("body")
+    .append("div")
+    .attr("class", "div-rect")
+
+const svgRect = d3.select(".div-rect")
+    .append("svg")
+    .attr("class", "svg-rect")
+    .attr("width", 300)
+    .attr("height", 100)
+
+svgRect.selectAll(".svg-rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("class", "rects")
+    .attr("x", (d, i) => i * 30)
+    .attr("y", (d, i) => parseInt(svgRect.attr("height")) - d)
+    .attr("width", 20)
+    .attr("height", (d => d))
